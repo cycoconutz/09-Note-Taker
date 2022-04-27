6b7f93ae1d427be15df22cb4acdca345c1009770
John Yates <johndyates@gmail.com>
2022-04-27 17:01:41 -0500

    home and notes loads

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+// const api = require('./public/assets/js/index.js');
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+app.use(express.static('public'));
+
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for homepage
+app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
